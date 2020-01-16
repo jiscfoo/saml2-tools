fb6924e016de895ba699ba526c36c2c9e1f15869
Matthew Slowe <matthew.slowe@jisc.ac.uk>
2020-01-16 14:23:54 +0000

    support both character encodings

---
+++ make-eptid-base32.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# echo -n 'entityID!username!salt' | openssl dgst -binary -sha1 | openssl base64
+_USAGE="Usage: $0 <input> [ <salt> | ENV:SALT ]"
+
+ENTITYID=${ENTITYID:-https://test.ukfederation.org.uk/entity}
+SOURCEID=${SOURCEID:-${1:?$_USAGE}}
+SALT=${SALT:-${2:?$_USAGE}}
+
+INPUT="${ENTITYID}!${SOURCEID}!${SALT}"
+
+echo "Input: $INPUT"
+echo -n "$INPUT" | openssl dgst -binary -sha1 | base32
